a59715726ca888ec60b31f149cc0211a695f3dfe
manish2397 <rawatmanish2397@gmail.com>
2019-09-04 23:32:41 +0530

    commands added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
-import pyperclip
-import os
+from commands.commands import getcommands
+
 r=sr.Recognizer()
 print(sr.Microphone.list_microphone_names())
 
@@ -8,17 +8,25 @@ print(sr.Microphone.list_microphone_names())
 with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source,duration=1)
     # r.energy_threshold = 600
-    print("say anything : ")
-    audio= r.record(source,duration=2)
-    with open("audio.wav","wb") as f:
-        f.write(audio.get_wav_data())
-    try:
-        text = r.recognize_google(audio)
-        pyperclip.copy(text)
-        print(text)
-        pyperclip.copy(text)
-        print(pyperclip.paste(text))
+
+    cnt=0
+    text=""
+    while True:
+        cnt+=1
+        if(cnt%10==0):
+            print("say anything : ",cnt)
+            audio = r.record(source, duration=5)
+            with open("audio.wav", "wb") as f:
+                f.write(audio.get_wav_data())
+            try:
+                recordedtext = r.recognize_google(audio)
+                print(recordedtext)
+                if(recordedtext.split(" ")[0]=="command"):
+                    getcommands(recordedtext,text)
+                else:
+                    text+=" "
+                    text+=recordedtext
+            except:
+                print("sorry, could not recognise")
 
 
-    except:
-        print("sorry, could not recognise")
